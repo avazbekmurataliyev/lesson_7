793aecac2a0f484b00bfe6c2cf03936bf02fc9f6
avazbekmurataliyev <97029745+avazbekmurataliyev@users.noreply.github.com>
2022-05-12 22:15:47 +0500

    Delete map directory

--- map/repository/DistrictRepository.java
+++
@@ -1,7 +0,0 @@
-package uz.avazbek.map.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.avazbek.map.entity.District;
-
-public interface DistrictRepository extends JpaRepository<District,Integer> {
-}
