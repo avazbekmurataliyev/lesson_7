567dded108b5b8791b204c9e093d1ee57c663316
avazbekmurataliyev <97029745+avazbekmurataliyev@users.noreply.github.com>
2022-05-12 22:16:26 +0500

    Delete map directory

--- map/repository/CountryRepository.java
+++
@@ -1,8 +0,0 @@
-package uz.avazbek.map.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.avazbek.map.entity.Country;
-
-public interface CountryRepository extends JpaRepository<Country,Integer> {
-
-}
